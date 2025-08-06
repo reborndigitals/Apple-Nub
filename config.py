745101298f36fde6315edad301bb0048e6d8f579
reborndigitals <reborndigitals@gmail.com>
2025-08-06 11:28:55 +0530

    config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ from fonts import *
 # Use getenv for all sensitive/configurable values
 API_ID = os.getenv("API_ID","10284859")
 API_HASH = os.getenv("API_HASH", "b0ad58eb8b845ba0003e0d9ce5fc2196")
-STRING_SESSION = os.getenv("STRING_SESSION", "BQDMBnkAhl63LfVwI1-Pu38Cj_U5J1fg94CoUuYa8VcOfu6JcKTsMf6IjdKxGuJUmTyLcycSMFw4H_amfXQ-6B8q0DoT1uA7xM4nAxR8XBQvDkepomQoB9qF6fiTRTBMJjIXFfVgNUY_5JI9PK08mJ26whDradE6lMEPlvVcR4Jto3KdaLpvTPE45nmx5VeAUl_LAgy4o3VgOa9S0yKy8Ha8d0ueGa_A8goiXm52ADheVq2EOcCuu09e-kJixlMujIuDrqLFd4S_RLlfTSvvmYAF0eOu1b6jz1vOwFahGBQCOT_eucH26KQe-VUlWlT-ECD3LyRJIwezaSrYZADjw_aXMUqQaQAAAAHjb-HaAA")
+STRING_SESSION = os.getenv("STRING_SESSION", "BQGJMTYAnMTEDXmgXzuptSLaeffcIr5AWGiw6J9jtZg94qowD2Y9tHBUaiWAIXtJr9PE2zDRaa5NWFPET5LXfEfxF8bOOdutrPcelB0sVdbQAThRjmyU_k1IhykbZI6vUlaQrFRqtD_p0uMPIXZqQoz8shZIIHkcbaMwN6Ee-x3xhYdTPEWFmSQGNoUEPFBSVPC0H4pN1Gc4XQaG1mApyMFhspCfs8txWLGaATqjskiVVSaH_Udc4-qTd6vT3h6UTdQcvrh76-j0RenbD5CttYWDlZe4uq9fT8SWMCQEMQKnMXD3LBROBq9GgDg8iuwPeGHd6jUriKFbWsaOiI0EZAMA3lu1GQAAAAHSHPeVAA")
 GROUP = os.getenv("GROUP", "HeartBeat_Fam")
 CHANNEL = os.getenv("CHANNEL", "HeartBeat_Offi")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8204653134:AAFLhxAIWEV937aucjUQP2T32W6DZsy8-QE")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "2096983652:AAHz41orhz9RrWscwg_WwSCp0_OhP-zLmDY")
 OWNER_ID = int(os.getenv("OWNER_ID", 1281282633))
 LOGGER_ID = os.getenv("LOGGER_ID", "-1001735663878")
 mongodb = os.getenv("MONGODB_URI", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
