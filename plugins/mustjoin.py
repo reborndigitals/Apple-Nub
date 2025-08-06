c5a422cebf9ef541f979775a7a8353f93eda498f
reborndigitals <reborndigitals@gmail.com>
2025-08-06 11:50:31 +0530

    mustjoin.py

--- plugins/mustjoin.py
+++ plugins/mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-import app
+from .. import app
 
 #--------------------------
 
